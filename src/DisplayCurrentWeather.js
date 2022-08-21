dbf2d64468c6b9a55e4b4b6d3272dcdbbe288aeb
Joey Sea <joey.c.sea@gmail.com>
2022-08-21 19:36:09 -0400

    Displayed hourly and 5 day forecast in their components 🌅

--- src/DisplayCurrentWeather.js
+++ src/DisplayCurrentWeather.js
@@ -1,29 +1,59 @@
-// DisplayWeather component
+// DisplayCurrentWeather component
 
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 const DisplayCurrentWeather = (props) => {
-   console.log(props.weatherResult);
+
+   const [ secondWeatherResult, setSecondWeatherResult] = useState(null);
+   // console.log(props.weatherResult);
+
+
+   // This will get the Hourly and 5 Day Weather data
+   const hourlyButtonClicked = () => {
+      axios({
+         url: `https://api.openweathermap.org/data/2.5/forecast/`,
+         method: "GET",
+         dataResponse: "json",
+         params: {
+            q: props.cityChoice,
+            appid: "3d828a8d5ff245862af24fb2c5883de1",
+            units: "metric",
+         }
+         }).then((res) => {
+            // console.log(res.data);
+            setSecondWeatherResult(res.data);
+         }).catch((error) => {
+         alert('Sorry there seems to be a problem!');
+      })
+   }
+
    return (
       <>
          <section>
-            <h3>Current Forecast</h3>
             {
                props.weatherResult !== null
                ?
                <>
+                  <h3>Current Forecast</h3>
                   <h4>{props.weatherResult.name}, {props.weatherResult.sys.country}</h4>
                   <p>{props.weatherResult.weather[0].description}</p>
                   <img src={`http://openweathermap.org/img/wn/${props.weatherResult.weather[0].icon}.png`} alt={props.weatherResult.weather[0].main} />
-                  <p>Humidity: {props.weatherResult.main.humidity}</p>
                   <p>Current Temp: {props.weatherResult.main.temp} ℃</p>
+                  <p>Feels Like: {props.weatherResult.main.feels_like} ℃</p>
                   <p>Highest Temp: {props.weatherResult.main.temp_max} ℃</p>
                   <p>Lowest Temp: {props.weatherResult.main.temp_min} ℃</p>
+                  <p>Humidity: {props.weatherResult.main.humidity}%</p>
+
+               <h4>Click for more weather information:</h4>
+                  <Link to="/hourly" state={secondWeatherResult}><button onClick={hourlyButtonClicked}>Hourly Forecast</button> </Link>
+                  <Link to ="/5days" state={secondWeatherResult}><button onClick={hourlyButtonClicked}>5 Day Forecast</button></Link>
                </>
                : null
             }
-         </section>
          <Outlet />
+         </section>
       </>
    )
 }
--- src/DisplayHourlyWeather.js
+++ src/DisplayHourlyWeather.js
@@ -1,9 +1,50 @@
 // DisplayHourlyWeather component
+import { useLocation } from 'react-router-dom';
 
-const DisplayHourlyWeather = () => {
+const DisplayHourlyWeather = (props) => {
+   const secondResult = useLocation();
+   const secondWeatherResult = secondResult.state;
+   console.log(secondWeatherResult);
+
    return (
       <section>
-         <h3>Hourly Forecast</h3>
+            {
+               secondWeatherResult !== null
+               ?
+               <>
+                  <h3>Hourly Forecast</h3>
+                  {
+                     <table>
+                        <tbody>
+                           <tr>
+                              <th>Every 3 Hours</th>
+                              <th>Temperature</th> 
+                           </tr>
+
+                           {secondWeatherResult.list.slice(0,9).map((item) => {
+                              return [
+                                 <tr key={item.dt}>
+                                    <td>
+                                       {item.dt_txt}
+                                    </td>
+                                    <td>
+                                       {item.main.temp} ℃
+                                    </td>
+                                    <td>
+                                       {item.weather[0].main}
+                                    </td>
+                                    <td>
+                                       {<img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`} alt={item.weather[0].main} />}
+                                    </td>
+                                 </tr>
+                              ]
+                           })}
+                        </tbody>
+                     </table>
+                  }
+               </>
+               : null
+            }
       </section>
    )
 }
